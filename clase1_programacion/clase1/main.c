3bdc0de0690d6b79165771372a6e20e4be04ba35
OscarAlvaroG <oscar_alvarogarcia@hotmail.com>
2017-08-15 10:13:55 -0300

    while por for
    cambio while por for

--- clase1_programacion/clase1/main.c
+++ clase1_programacion/clase1/main.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    //variables
+int main(){
+    // introducir 5 numeros y calcular su promedio
     int numero;
     int contador = 0;
-    int total_n = 0;
+    int total_n = 0 ;
     float promedio;
-    //bucle de 5 numeros
-    while(contador<5){
-        contador++;
+
+    for(contador = 0; contador < 5; contador++){
         printf("Ingrese un numero");
         scanf("%d", &numero);
         total_n = total_n + numero;
     }
-    // imprimo en pantalla
-    promedio = (float)total_n / contador;
-
-    printf("El promedio es: %.2f, total de numeros %i, cantidad de Numeros: %i", promedio, total_n, contador);
+    //promedio
+    promedio = (float) total_n / contador;
+    //imprimo
+    printf("El promedio es: %f", promedio);
 
     return 0;
 }
